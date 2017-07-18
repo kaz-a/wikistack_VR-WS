6294b1c1850b7611c5cf27f7a3ba79bd7656b0c7
rivermanvr <vrgithub@riversconsulting.com>
2017-07-17 22:20:29 -0400

    switched to nunjucks

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require( 'express' );
 const app = express();
 const path = require( 'path' );
+const nunjucks = require( 'nunjucks' );
+const bodyParser = require( 'body-parser' );
 const morgan = require( 'morgan' );
 
+nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
 
